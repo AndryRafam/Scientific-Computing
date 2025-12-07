a503946cea609faa6c98456f5b25217133df0237
Andry Rafam <41973268+AndryRafam@users.noreply.github.com>
2025-12-07 20:58:57 +0300

    Update trapezoidal_app.m

--- Integral/trapezoidal_app.m
+++ Integral/trapezoidal_app.m
@@ -1,1 +1,13 @@
+function trapezoidal_app()
+	v = @(t) 3*(t^2)*exp(t^3);
+	n = input("n:");
+	numerical = trapezoidal(v,0,1,n);
 
+	% Compare with exact result
+	V = @(t) exp(t^3); % dV/dt = v;
+	exact = V(1) - V(0); % exact value of the integral
+	fprintf("exact result: %.16f\n",exact);
+	fprintf("numerical: %.16f\n",numerical);
+	error = exact - numerical;
+	fprintf("error (exact-numerical): %g\n",error);
+end
