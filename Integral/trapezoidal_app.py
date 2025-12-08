74595fabc8b6683838bd0980489c9543a2c8df26
Andry Rafam <41973268+AndryRafam@users.noreply.github.com>
2025-12-08 09:12:54 +0300

    Update trapezoidal_app.py

--- Integral/trapezoidal_app.py
+++ Integral/trapezoidal_app.py
@@ -1,1 +1,17 @@
+from math import exp
+from math import pow
+from trapezoidal import trapezoidal
 
+def application():
+	v = lambda t: 3*(pow(t,2))*exp(pow(t,3))
+	n = int(input("n: "))
+	numerical = trapezoidal(v,0,1,n)
+
+	# Compare with exact result
+	V = lambda t: exp(pow(t,3)) # dV/dt = v
+	exact = V(1) - V(0)
+	error = exact - numerical
+	print("n=%d: %.16f, error: %g" % (n, numerical, error))
+
+if __name__ == "__main__":
+	application()
